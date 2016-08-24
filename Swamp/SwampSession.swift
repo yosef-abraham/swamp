efffa8bd6c5865ed2a9a00c8e0e4d5bea1d0bee4
Yossi Abraham <yo.ab@outlook.com>
2016-08-24 23:10:15 +0300

    refactor public API, implement subscribe/unsubscribe mechanism

--- Swamp/SwampSession.swift
+++ Swamp/SwampSession.swift
@@ -5,6 +5,41 @@
 import Foundation
 import SwiftyJSON
 
+// MARK: Call callbacks
+public typealias CallCallback = (details: [String: AnyObject], results: [AnyObject]?, kwResults: [String: AnyObject]?) -> Void
+public typealias ErrorCallCallback = (details: [String: AnyObject], error: String, args: [AnyObject]?, kwargs: [String: AnyObject]?) -> Void
+// MARK: Subscribe callbacks
+public typealias SubscribeCallback = (subscription: Subscription) -> Void
+public typealias ErrorSubscribeCallback = (details: [String: AnyObject], error: String) -> Void
+public typealias EventCallback = (details: [String: AnyObject], results: [AnyObject]?, kwResults: [String: AnyObject]?) -> Void
+public typealias UnsubscribeCallback = () -> Void
+public typealias ErrorUnsubscribeCallback = (details: [String: AnyObject], error: String) -> Void
+
+
+public class Subscription {
+    private let session: SwampSession
+    internal let subscription: Int
+    internal let eventCallback: EventCallback
+    private var isActive: Bool = true
+    
+    internal init(session: SwampSession, subscription: Int, onEvent: EventCallback) {
+        self.session = session
+        self.subscription = subscription
+        self.eventCallback = onEvent
+    }
+    
+    internal func invalidate() {
+        self.isActive = false
+    }
+    
+    public func cancel(onSuccess: UnsubscribeCallback, onError: ErrorUnsubscribeCallback) {
+        if !self.isActive {
+            onError(details: [:], error: "Subscription already inactive.")
+        }
+        self.session.unsubscribe(self.subscription, onSuccess: onSuccess, onError: onError)
+    }
+}
+
 public protocol SwampSessionDelegate {
     func swampSessionHandleChallenge(authMethod: String, extra: [String: AnyObject]) -> String
     func swampSessionConnected(session: SwampSession, sessionId: Int)
@@ -13,30 +48,39 @@ public protocol SwampSessionDelegate {
 
 public class SwampSession: SwampTransportDelegate {
     // MARK: Public typealiases
-    //                               details              results      kwResults
-    public typealias CallCallback = ([String: AnyObject], [AnyObject]?, [String: AnyObject]?) -> Void
-    //                                    details              uri     args          kwargs
-    public typealias ErrorCallCallback = ([String: AnyObject], String, [AnyObject]?, [String: AnyObject]?) -> Void
     
     // MARK: delegate
     public var delegate: SwampSessionDelegate?
     
     // MARK: Constants
     private let supportedRoles: [SwampRole] = [SwampRole.Caller, SwampRole.Callee, SwampRole.Subscriber, SwampRole.Publisher]
+    private let clientName = "Swamp-dev-0.1.0"
     
     // MARK: Members
-    private var realm: String
-    private var transport: SwampTransport
-    private var details: [String: AnyObject]
+    private let realm: String
+    private let transport: SwampTransport
+    private let details: [String: AnyObject]
     
+    // MARK: State members
+    private var currRequestId: Int = 1
+    
+    // MARK: Session state
     private var serializer: SwampSerializer?
     private var sessionId: Int?
     private var routerSupportedRoles: [SwampRole]?
     
-    // MARK: State members
-    private var currRequestId: Int = 1
-    private var callRequests: [Int: (callback: CallCallback?, errorCallback: ErrorCallCallback?)] = [:]
+    // MARK: Call role
+    //                         requestId
+    private var callRequests: [Int: (callback: CallCallback, errorCallback: ErrorCallCallback)] = [:]
+    // MARK: Subscribe role
+    //                              requestId
+    private var subscribeRequests: [Int: (callback: SubscribeCallback, errorCallback: ErrorSubscribeCallback, eventCallback: EventCallback)] = [:]
+    //                          subscription
+    private var subscriptions: [Int: Subscription] = [:]
+    //                                requestId
+    private var unsubscribeRequests: [Int: (subscription: Int, callback: UnsubscribeCallback, errorCallback: ErrorUnsubscribeCallback)] = [:]
     
+    // MARK: C'tor
     public init(realm: String, transport: SwampTransport, details: [String: AnyObject]=[:]){
         self.realm = realm
         self.transport = transport
@@ -56,12 +100,32 @@ public class SwampSession: SwampTransportDelegate {
     
     // MARK: Caller role
     
-    public func call(uri: String, options: [String: AnyObject], args: [AnyObject]?=nil, kwargs: [String: AnyObject]?=nil, callback: CallCallback?, errorCallback: ErrorCallCallback?) {
+    public func call(proc: String, options: [String: AnyObject]=[:], args: [AnyObject]?=nil, kwargs: [String: AnyObject]?=nil, onSuccess: CallCallback, onError: ErrorCallCallback) {
         let callRequestId = self.generateRequestId()
         // Tell router to dispatch call
-        self.sendMessage(CallSwampMessage(requestId: callRequestId, options: options, proc: uri, args: args, kwargs: kwargs))
+        self.sendMessage(CallSwampMessage(requestId: callRequestId, options: options, proc: proc, args: args, kwargs: kwargs))
         // Store request ID to handle result
-        self.callRequests[callRequestId] = (callback: callback, errorCallback: errorCallback)
+        self.callRequests[callRequestId] = (callback: onSuccess, errorCallback: onError)
+    }
+    
+    // MARK: Subscriber role
+    
+    public func subscribe(topic: String, options: [String: AnyObject]=[:], onSuccess: SubscribeCallback, onError: ErrorSubscribeCallback, onEvent: EventCallback) {
+        // TODO: assert topic is a valid WAMP uri
+        let subscribeRequestId = self.generateRequestId()
+        // Tell router to subscribe client on a topic
+        self.sendMessage(SubscribeSwampMessage(requestId: subscribeRequestId, options: options, topic: topic))
+        // Store request ID to handle result
+        self.subscribeRequests[subscribeRequestId] = (callback: onSuccess, errorCallback: onError, eventCallback: onEvent)
+    }
+    
+    // Internal because only a Subscription object can call this
+    internal func unsubscribe(subscription: Int, onSuccess: UnsubscribeCallback, onError: ErrorUnsubscribeCallback) {
+        let unsubscribeRequestId = self.generateRequestId()
+        // Tell router to unsubscribe me from some subscription
+        self.sendMessage(UnsubscribeSwampMessage(requestId: unsubscribeRequestId, subscription: subscription))
+        // Store request ID to handle result
+        self.unsubscribeRequests[unsubscribeRequestId] = (subscription, onSuccess, onError)
     }
     
     // MARK: SwampTransportDelegate
@@ -86,8 +150,9 @@ public class SwampSession: SwampTransportDelegate {
             // For now basic profile, (demands empty dicts)
             roles[role.rawValue] = [:]
         }
-        self.details["agent"] = "Swamp-dev-0.1.0"
-        self.details["roles"] = roles
+        var details = self.details
+        details["agent"] = self.clientName
+        details["roles"] = roles
         self.sendMessage(HelloSwampMessage(realm: self.realm, details: self.details))
     }
     
@@ -99,6 +164,7 @@ public class SwampSession: SwampTransportDelegate {
     
     private func handleMessage(message: SwampMessage) {
         switch message {
+        // MARK: Auth responses
         case let message as ChallengeSwampMessage:
             if let authResponse = self.delegate?.swampSessionHandleChallenge(message.authMethod, extra: message.extra) {
                 self.sendMessage(AuthenticateSwampMessage(signature: authResponse, extra: [:]))
@@ -106,6 +172,7 @@ public class SwampSession: SwampTransportDelegate {
                 print("There was no delegate, aborting.")
                 try! self.abort()
             }
+        // MARK: Session responses
         case let message as WelcomeSwampMessage:
             self.sessionId = message.sessionId
             let routerRoles = message.details["roles"]! as! [String : [String : AnyObject]]
@@ -119,26 +186,70 @@ public class SwampSession: SwampTransportDelegate {
             self.transport.disconnect(message.reason)
         case let message as AbortSwampMessage:
             self.transport.disconnect(message.reason)
+        // MARK: Call role
+        case let message as ResultSwampMessage:
+            let requestId = message.requestId
+            if let (callback, errorCallback) = self.callRequests.removeValueForKey(requestId) {
+                callback(details: message.details, results: message.results, kwResults: message.kwResults)
+            } else {
+                // TODO: log this erroneous situation
+            }
+        // MARK: Subscribe role
+        case let message as SubscribedSwampMessage:
+            let requestId = message.requestId
+            if let (callback, _, eventCallback) = self.subscribeRequests.removeValueForKey(requestId) {
+                // Notify user and delegate him to unsubscribe this subscription
+                let subscription = Subscription(session: self, subscription: message.subscription, onEvent: eventCallback)
+                callback(subscription: subscription)
+                // Subscription succeeded, we should store event callback for when it's fired
+                self.subscriptions[message.subscription] = subscription
+            } else {
+                // TODO: log this erroneous situation
+            }
+        case let message as EventSwampMessage:
+            if let subscription = self.subscriptions[message.subscription] {
+                subscription.eventCallback(details: message.details, results: message.args, kwResults: message.kwargs)
+            } else {
+                // TODO: log this erroneous situation
+            }
+        case let message as UnsubscribedSwampMessage:
+            let requestId = message.requestId
+            if let (subscription, callback, _) = self.unsubscribeRequests.removeValueForKey(requestId) {
+                if let subscription = self.subscriptions.removeValueForKey(subscription) {
+                    subscription.invalidate()
+                } else {
+                    // TODO: log this erroneous situation
+                }
+            } else {
+                // TODO: log this erroneous situation
+            }
+            
+        ////////////////////////////////////////////
+        // MARK: Handle error responses
+        ////////////////////////////////////////////
         case let message as ErrorSwampMessage:
             switch message.requestType {
             case SwampMessages.Call:
-                let requestId = message.requestId
-                if let (callback, errorCallback) = self.callRequests[requestId] {
-                    if errorCallback != nil {
-                        errorCallback!(message.details, message.error, message.args, message.kwargs)
-                    }
+                if let (callback, errorCallback) = self.callRequests.removeValueForKey(message.requestId) {
+                    errorCallback(details: message.details, error: message.error, args: message.args, kwargs: message.kwargs)
+                } else {
+                    // TODO: log this erroneous situation
+                }
+            case SwampMessages.Subscribe:
+                if let (_, errorCallback, _) = self.subscribeRequests.removeValueForKey(message.requestId) {
+                    errorCallback(details: message.details, error: message.error)
+                } else {
+                    // TODO: log this erroneous situation
+                }
+            case SwampMessages.Unsubscribe:
+                if let (_, _, errorCallback) = self.unsubscribeRequests.removeValueForKey(message.requestId) {
+                    errorCallback(details: message.details, error: message.error)
+                } else {
+                    // TODO: log this erroneous situation
                 }
             default:
                 return
             }
-        case let message as ResultSwampMessage:
-            let requestId = message.requestId
-            if let (callback, errorCallback) = self.callRequests[requestId] {
-                if callback != nil {
-                    callback!(message.details, message.results, message.kwResults)
-                }
-            }
-            
         default:
             return
         }
