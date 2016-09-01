257fd6644f4f13dd62c0d4025a8187e1606c8e87
Yossi Abraham <yo.ab@outlook.com>
2016-09-01 12:28:31 +0300

    Adjusting test scripts to run docker properly... adding a python component for testing

---
+++ Example/node/OpenRealmSession.py
@@ -0,0 +1,22 @@
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import LoopingCall
+from autobahn.twisted.wamp import ApplicationSession
+
+class OpenRealmSession(ApplicationSession):
+    @inlineCallbacks
+    def onJoin(self, details):
+        def heartbeat():
+            return self.publish(u'org.swamp.heartbeat', 'Heartbeat!')
+
+        LoopingCall(heartbeat).start(1)
+
+        def add(num1, num2):
+            return num1 + num2
+
+        yield self.register(add, u"org.swamp.add")
+
+        def echo(param1, param2):
+            return param1, param2
+
+        yield self.register(add, u"org.swamp.echo")
+    
