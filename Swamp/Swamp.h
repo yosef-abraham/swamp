bf1c245827fd939b9d501aceb40594e0f05c220b
Eli Burke <eburke@pobox.com>
2016-10-17 10:22:33 -0400

    Added Framework project with shared schemes for Carthage support

---
+++ Swamp/Swamp.h
@@ -0,0 +1,17 @@
+//
+//  Swamp.h
+//  Swamp
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swamp.
+FOUNDATION_EXPORT double SwampVersionNumber;
+
+//! Project version string for Swamp.
+FOUNDATION_EXPORT const unsigned char SwampVersionString[];
+
+// In this header, you should import all the public headers of your framework 
+// using statements like #import <Swamp/PublicHeader.h>
+
+
